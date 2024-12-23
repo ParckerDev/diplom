ee3e0d4f790a4cafecb769edbc5d1c2d75103041
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-24 00:18:41 +0600

    add naming convention in Settings

--- fastapi_app/core/config.py
+++ fastapi_app/core/config.py
@@ -4,6 +4,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     db_url = "sqlite+aiosqlite:///./fa_rent_db.sqlite3"
     echo = True
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 settings = Settings()
