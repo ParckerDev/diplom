48cd71261c91882d044d7d9bd30f83413cb20168
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-24 00:19:36 +0600

    add in Base model table_naming and naming convention

--- fastapi_app/core/base.py
+++ fastapi_app/core/base.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from . import settings
 
 
 class Base(DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.naming_convention,
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
