134c2903863b8a6c2b61820ad9183348add54c5e
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-23 21:07:14 +0600

    create views

--- django_app/rent_eq/views.py
+++ django_app/rent_eq/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Rental, Equipment
+from .serializers import UserSerializer, RentalSerializer, EquipmentSerializer
+
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class RentalViewSet(viewsets.ModelViewSet):
+    queryset = Rental.objects.all()
+    serializer_class = RentalSerializer
+
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
