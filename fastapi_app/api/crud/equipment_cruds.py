cff4674321990f1157b1a21772d4a73f4ba7133e
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-24 13:24:49 +0600

    create crud and routes for equipment

---
+++ fastapi_app/api/crud/equipment_cruds.py
@@ -0,0 +1,45 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, delete, update
+from models import Equipment
+from schemas.equipment import EquipmentBase, EquipmentCreate
+
+
+async def create_equipment(new_equipment: EquipmentCreate, session: AsyncSession) -> Equipment:
+    equipment = EquipmentCreate(**new_equipment.model_dump())
+    session.add(equipment)
+    await session.commit()
+    await session.refresh(equipment)
+    return equipment
+
+async def get_all_equipment(session: AsyncSession) -> list[Equipment]:
+    stmt = select(Equipment).order_by(Equipment.id)
+    equipment_list = await session.scalars(stmt)
+    return equipment_list.all()
+
+
+async def get_equipment_by_id(session: AsyncSession, equipment_id: int) -> Equipment:
+    stmt = select(Equipment).where(Equipment.id == equipment_id)
+    equipment = await session.scalar(stmt)
+    return equipment
+
+
+async def update_equipment(equipment_id: int, equipment_data: EquipmentBase, session: AsyncSession) -> Equipment:
+    stmt = (
+        update(Equipment)
+        .where(Equipment.id == equipment_id)
+        .values(**equipment_data.model_dump())
+        .execution_options(synchronize_session="fetch")
+    )
+    await session.execute(stmt)
+    await session.commit()
+    
+    # Получаем обновленное оборудование
+    updated_equipment = await session.get(Equipment, equipment_id)
+    return updated_equipment
+
+
+async def delete_equipment(session: AsyncSession, equipment_id: int) -> bool:
+    stmt = delete(Equipment).where(Equipment.id == equipment_id)
+    result = await session.execute(stmt)
+    await session.commit()
+    return result.rowcount > 0
---
+++ fastapi_app/api/equipment.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from core.db_help import db_helper
+from schemas.equipment import EquipmentBase, EquipmentCreate
+from .crud.equipment_cruds import (
+    create_equipment,
+    delete_equipment,
+    get_all_equipment,
+    get_equipment_by_id,
+    update_equipment,
+)
+
+router = APIRouter(prefix="equipment/", tags=["Equipment"])
+conn = Annotated[AsyncSession, Depends(db_helper.get_session)]
+
+
+@router.get("", response_model=list[EquipmentBase])
+async def get_equipments(session=conn):
+    equipments = await get_all_equipment(session=session)
+    return equipments
+
+
+@router.post("", response_model=EquipmentBase)
+async def create_equipment_endpoint(new_equipment: EquipmentCreate, session: conn):
+    equipment = await create_equipment(new_equipment=new_equipment, session=session)
+    return equipment
+
+
+@router.get("/{equipment_id}", response_model=EquipmentBase)
+async def get_equipment_by_id_endpoint(equipment_id: int, session: conn):
+    equipment = await get_equipment_by_id(session=session, equipment_id=equipment_id)
+    if equipment is None:
+        raise HTTPException(status_code=404, detail="Equipment not found")
+    return equipment
+
+
+@router.put("/{equipment_id}", response_model=EquipmentBase)
+async def update_equipment_endpoint(
+    equipment_id: int, equipment_data: EquipmentBase, session: conn
+):
+    updated_equipment = await update_equipment(
+        equipment_id=equipment_id, equipment_data=equipment_data, session=session
+    )
+    if updated_equipment is None:
+        raise HTTPException(status_code=404, detail="Equipment not found")
+    return updated_equipment
+
+
+@router.delete("/{equipment_id}", response_model=dict)
+async def delete_equipment_endpoint(equipment_id: int, session: conn):
+    deleted = await delete_equipment(session=session, equipment_id=equipment_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Equipment not found")
+    return {"detail": "Equipment deleted successfully"}
