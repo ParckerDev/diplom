585e74082a887dd2e2fbabc383174878b342d69c
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-24 14:08:47 +0600

    rename class from Rental to Rental_id

--- fastapi_app/schemas/rental.py
+++ fastapi_app/schemas/rental.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 from pydantic import BaseModel
 
+
 class RentalBase(BaseModel):
     equipment_id: int
     user_id: int
     start_date: datetime
     end_date: datetime
 
+
 class RentalCreate(RentalBase):
     pass
 
-class Rental(RentalBase):
+
+class Rental_id(RentalBase):
     id: int
 
     class Config:
-        orm_model=True
+        orm_model = True
