6c3a0e3761e69e41c60b5c10958b6e67f8ccfd4f
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-24 13:29:41 +0600

    create main.py

---
+++ fastapi_app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter
+from api import router as api_router
+
+
+app = FastAPI()
+
+app.include_router(api_router)
+
+
+@app.get("")
+async def index():
+    return {"message": "api rental equipments"}
+
+
+if __name__=='__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
