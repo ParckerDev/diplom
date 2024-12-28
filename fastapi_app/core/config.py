620a8a28cc13032d48b3cb9a42794ea7055b7329
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-28 23:50:11 +0600

    update db url

--- fastapi_app/core/config.py
+++ fastapi_app/core/config.py
@@ -1,8 +1,9 @@
 from pydantic_settings import BaseSettings
 
 
+
 class Settings(BaseSettings):
-    db_url: str = "sqlite+aiosqlite:///fa_rent_db.sqlite3"
+    db_url: str = "sqlite+aiosqlite:///./fa_rent_db.sqlite3"
     echo:bool = True
     naming_convention: dict[str, str] = {
         "ix": "ix_%(column_0_label)s",
