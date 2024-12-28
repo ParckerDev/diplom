29a8def4d3fc68702bbf1714ec2a87640d5f6966
Alexandr Shevchenko <djpestro@gmail.com>
2024-12-28 22:20:12 +0600

    add rentals api in main api

--- fastapi_app/api/__init__.py
+++ fastapi_app/api/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .user import router as user_router
 from .equipment import router as equipment_router
+from .rentals import router as rentals_router
 
-router = APIRouter(prefix='/api')
+router = APIRouter(prefix="/api")
 
 router.include_router(user_router)
 router.include_router(equipment_router)
+router.include_router(rentals_router)
