31570fc729932ae81f7b8a718b3e13c3fe4222c4
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-03 22:49:00 +0600

    update equipment schema

--- flask_app/schemas/equipment_schema.py
+++ flask_app/schemas/equipment_schema.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel
 
+
 class EquipmentCreate(BaseModel):
     name: str
     description: str | None
     cost: int
 
-class EquipmentResponse(BaseModel):
+
+class EquipmentResponse(EquipmentCreate):
     id: int
-    name: str
-    description: str | None
-    cost: int
 
     class Config:
         from_attributes = True
