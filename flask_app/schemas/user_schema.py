edf9f4bc813e1c49c8504855e4ad20d246178a26
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-03 22:46:02 +0600

    create user schema

---
+++ flask_app/schemas/user_schema.py
@@ -0,0 +1,14 @@
+# schemas/user_schema.py
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        from_attributes = True
