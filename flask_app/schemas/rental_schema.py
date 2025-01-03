ef38289ce96fcaab6b272d91059b8a71b04a78f8
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-03 22:48:15 +0600

    update rental schema

--- flask_app/schemas/rental_schema.py
+++ flask_app/schemas/rental_schema.py
@@ -1,17 +1,16 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class RentalCreate(BaseModel):
-    user_id: int
-    start_date: datetime
-    end_date: datetime
 
-class RentalResponse(BaseModel):
-    id: int
+class RentalCreate(BaseModel):
     equipment_id: int
     user_id: int
     start_date: datetime
     end_date: datetime
+
+
+class RentalResponse(RentalCreate):
+    id: int
 
     class Config:
         from_attributes = True
