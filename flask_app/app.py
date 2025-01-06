7d535f8fccf9c2c7b7b30c03eb0bc61de06c4aa3
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-07 03:21:20 +0600

    add resource in app file

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from resources.user_resource import UserResource, UserListResource
+from resources.equipment_resources import EquipmentResource, EquipmentListResource
+from resources.rental_resource import RentalResource, RentalListResource
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rentals.db"
@@ -8,6 +11,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 api = Api(app)
+
+
+api.add_resource(UserListResource, "/users")
+api.add_resource(UserResource, "/users/<int:user_id>")
+api.add_resource(EquipmentListResource, "/equipment")
+api.add_resource(EquipmentResource, "/equipment/<int:equipment_id>")
+api.add_resource(RentalListResource, "/rentals")
+api.add_resource(RentalResource, "/rentals/<int:rental_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
