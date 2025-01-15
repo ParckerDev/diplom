01df4ce4e779d77f462670a61785371d403971ad
John Shevchenko <John_Shevchenko@gmail.com>
2025-01-15 13:38:21 +0300

    add docstrings in serializers.py

--- django_app/rent_eq/serializers.py
+++ django_app/rent_eq/serializers.py
@@ -3,20 +3,40 @@ from .models import Equipment, User, Rental
 
 
 class BaseSerializer(serializers.ModelSerializer):
+    """
+    Базовый сериализатор для моделей, наследующий от ModelSerializer.
+
+    Этот сериализатор автоматически включает все поля модели в сериализацию и десериализацию.
+    """
     class Meta:
         fields = "__all__"
 
 
 class UserSerializer(BaseSerializer):
+    """
+    Сериализатор для модели User.
+
+    Используется для преобразования экземпляров модели User в JSON и обратно.
+    """
     class Meta(BaseSerializer.Meta):
         model = User
 
 
 class EquipmentSerializer(BaseSerializer):
+    """
+    Сериализатор для модели Equipment.
+
+    Используется для преобразования экземпляров модели Equipment в JSON и обратно.
+    """
     class Meta(BaseSerializer.Meta):
         model = Equipment
 
 
 class RentalSerializer(BaseSerializer):
+    """
+    Сериализатор для модели Rental.
+
+    Используется для преобразования экземпляров модели Rental в JSON и обратно.
+    """
     class Meta(BaseSerializer.Meta):
         model = Rental
