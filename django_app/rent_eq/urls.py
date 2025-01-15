5dbdfc19ae471918e57fb90e3ffb2dc353461160
John Shevchenko <John_Shevchenko@gmail.com>
2025-01-15 13:40:37 +0300

    add comment in urls.py

--- django_app/rent_eq/urls.py
+++ django_app/rent_eq/urls.py
@@ -2,13 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EquipmentViewSet, RentalViewSet, UserViewSet
 
+# Создаем экземпляр маршрутизатора по умолчанию
 router = DefaultRouter()
 
-router.register(r"users", UserViewSet)
-router.register(r"rental", RentalViewSet)
-router.register(r"equipment", EquipmentViewSet)
+# Регистрируем маршруты для представлений (ViewSet) пользователей, аренды и оборудования
+router.register(r"users", UserViewSet)        # Маршрут для пользователей
+router.register(r"rental", RentalViewSet)      # Маршрут для аренды
+router.register(r"equipment", EquipmentViewSet) # Маршрут для оборудования
 
-
+# Определяем URL-шаблоны для приложения
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include(router.urls)),  # Включаем маршруты, определенные в маршрутизаторе
 ]
