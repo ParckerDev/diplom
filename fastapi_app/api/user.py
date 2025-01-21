532ed0e16d8837cc90e963096fc229fabf29e0b2
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-21 20:37:08 +0600

    add docstring in user router

--- fastapi_app/api/user.py
+++ fastapi_app/api/user.py
@@ -17,24 +17,69 @@ conn = Annotated[AsyncSession, Depends(db_helper.get_session)]
 
 @router.post("", response_model=User_id)
 async def user_create(new_user: UserCreate, session: conn):
+    """
+    Создает нового пользователя.
+
+    Аргументы:
+        new_user (UserCreate): Данные нового пользователя.
+        session (AsyncSession): Асинхронная сессия для работы с базой данных.
+
+    Возвращает:
+        User_id: Созданный пользователь с его идентификатором.
+    """
     user = await create_user(new_user, session)
     return user
 
 
 @router.get("", response_model=list[User_id])
 async def get_all_users(session: conn):
+    """
+    Получает список всех пользователей.
+
+    Аргументы:
+        session (AsyncSession): Асинхронная сессия для работы с базой данных.
+
+    Возвращает:
+        list[User_id]: Список пользователей.
+    """
     users = await get_users(session=session)
     return users
 
 
 @router.get("/{user_id}", response_model=User_id)
 async def get_user_id(session: conn, user_id: int):
+    """
+    Получает пользователя по его идентификатору.
+
+    Аргументы:
+        session (AsyncSession): Асинхронная сессия для работы с базой данных.
+        user_id (int): Идентификатор пользователя.
+
+    Возвращает:
+        User_id: Данные пользователя.
+
+    Исключения:
+        HTTPException: Если пользователь не найден, возвращает 404.
+    """
     user = await get_user_by_id(session=session, user_id=user_id)
     return user
 
 
 @router.delete("/{user_id}", response_model=dict)
-async def del_user(session: conn, user_id):
+async def del_user(session: conn, user_id: int):
+    """
+    Удаляет пользователя по его идентификатору.
+
+    Аргументы:
+        session (AsyncSession): Асинхронная сессия для работы с базой данных.
+        user_id (int): Идентификатор пользователя для удаления.
+
+    Возвращает:
+        dict: Сообщение об успешном удалении.
+
+    Исключения:
+        HTTPException: Если пользователь не найден, возвращает 404.
+    """
     deleted = await delete_user(session=session, user_id=user_id)
     if not deleted:
         raise HTTPException(status_code=404, detail="User not found!")
@@ -43,6 +88,20 @@ async def del_user(session: conn, user_id):
 
 @router.put("/{user_id}", response_model=User_id)
 async def update_user(session: conn, user_id: int, user_data: UserBase):
+    """
+    Обновляет данные пользователя по его идентификатору.
+
+    Аргументы:
+        session (AsyncSession): Асинхронная сессия для работы с базой данных.
+        user_id (int): Идентификатор пользователя для обновления.
+        user_data (UserBase): Новые данные пользователя.
+
+    Возвращает:
+        User_id: Обновленные данные пользователя.
+
+    Исключения:
+        HTTPException: Если пользователь не найден, возвращает 404.
+    """
     updated_user = await user_update(
         session=session, user_id=user_id, user_data=user_data
     )
