d29e32e448057fb92c361da4c8756bc6b102430f
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-21 20:36:13 +0600

    add  docstring in models

--- fastapi_app/models.py
+++ fastapi_app/models.py
@@ -4,6 +4,16 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class User(Base):
+    """
+    Модель пользователя.
+
+    Атрибуты:
+        username (str): Уникальное имя пользователя.
+        email (str): Уникальный адрес электронной почты.
+        password (str): Пароль пользователя.
+        telephone_number (str): Номер телефона пользователя.
+    """
+
     username: Mapped[str] = mapped_column(
         String(100), unique=True, nullable=False, index=True
     )
@@ -14,18 +24,48 @@ class User(Base):
     telephone_number: Mapped[str] = mapped_column(String(12), nullable=False)
 
     def __str__(self):
+        """
+        Возвращает строковое представление пользователя.
+
+        Возвращает:
+            str: Строка, содержащая имя пользователя и номер телефона.
+        """
         return f"{self.username} | {self.telephone_number}"
 
 
 class Equipment(Base):
+    """
+    Модель оборудования.
+
+    Атрибуты:
+        name (str): Уникальное название оборудования.
+        discription (str): Описание оборудования.
+    """
+
     name: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
     discription: Mapped[str] = mapped_column(String(250))
 
     def __str__(self):
+        """
+        Возвращает строковое представление оборудования.
+
+        Возвращает:
+            str: Название оборудования.
+        """
         return self.name
 
 
 class Rental(Base):
+    """
+    Модель аренды.
+
+    Атрибуты:
+        equipment_id (int): Идентификатор арендуемого оборудования.
+        user_id (int): Идентификатор пользователя, который арендует оборудование.
+        start_date (str): Дата начала аренды.
+        end_date (str): Дата окончания аренды.
+    """
+
     equipment_id: Mapped[int] = mapped_column(Integer, ForeignKey("equipments.id"))
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     start_date: Mapped[str] = mapped_column(Date)
